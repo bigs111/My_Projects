c0b6626f0f08fb8ec6e8a8bac808c5b1b8ae0571
bigs111 <128871502+bigs111@users.noreply.github.com>
2023-09-02 11:34:39 -0400

    New Projects

---
+++ christmas lights Web/main.js
@@ -0,0 +1,40 @@
+/* 
+Drawing circles
+*/
+
+// light one
+var one = document.getElementById("lightOne");
+var ctxOne = one.getContext("2d");
+ctxOne.beginPath();
+ctxOne.arc(95, 50, 40, 0, 2 * Math.PI);
+ctxOne.stroke();
+
+// light two
+var two = document.getElementById("lightTwo");
+var ctxTwo = two.getContext("2d");
+ctxTwo.beginPath();
+ctxTwo.arc(95, 50, 40, 0, 2 * Math.PI);
+ctxTwo.stroke();
+
+// light three
+var three = document.getElementById("lightThree");
+var ctxThree = three.getContext("2d");
+ctxThree.beginPath();
+ctxThree.arc(95, 50, 40, 0, 2 * Math.PI);
+ctxThree.stroke();
+
+// light 4 
+var four = document.getElementById("lightFour");
+var ctxFour = four.getContext("2d");
+ctxFour.beginPath();
+ctxFour.arc(95, 50, 40, 0, 2 * Math.PI);
+ctxFour.stroke(); 
+
+// light 5 
+var five = document.getElementById("lightFive");
+var ctxFive = five.getContext("2d");
+ctxFive.beginPath();
+ctxFive.arc(95, 50, 40, 0, 2 * Math.PI);
+ctxFive.stroke();
+
+// light 6 
